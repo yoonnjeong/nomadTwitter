0433401d8f8db714884938cb41ca16d55e902ca2
SEOYOONJEONG-ez <112161928+yoonjeongseo@users.noreply.github.com>
2023-12-06 17:46:58 +0900

    delete tweets 코멘트 추가

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -46,16 +46,17 @@ export default function Tweets({ username, photo, tweet, userId, id }: ITweet) {
   const user = auth.currentUser;
   const onDelete = async () => {
     const ok = confirm("Are you sure you want to delete this tweet?");
-    if (!ok || user?.uid !== userId) return;
-    if (user?.uid !== userId) return;
+    if (!ok || user?.uid !== userId) return; // 확인 버튼을 누를 경우에만 삭제
+    if (user?.uid !== userId) return; // 사용자 아이디가 다를 경우 삭제 못함
     try {
       await deleteDoc(doc(db, "tweets", id));
       if (photo) {
         const photoRef = ref(
+          // 사진 경로 받아오기
           storage,
           `tweets/${user.uid}-${user.displayName}/${id}`
         );
-        await deleteObject(photoRef);
+        await deleteObject(photoRef); // 사진 삭제
       }
     } catch (error) {
       console.log(error);

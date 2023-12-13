b84c0fe6be73e08717aa44efa1c17ba0009872ae
SEOYOONJEONG-ez <112161928+yoonjeongseo@users.noreply.github.com>
2023-12-13 16:12:03 +0900

    profile text 변경

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -12,6 +12,7 @@ import {
   where,
 } from "firebase/firestore";
 import Tweet from "../components/tweet";
+import { Error } from "../components/auth-components";
 
 const Wrapper = styled.div`
   display: flex;
@@ -49,11 +50,55 @@ const Tweets = styled.div`
   width: 100%;
   margin-bottom: 100px;
 `;
+const EditNameTextEField = styled.textarea`
+  border: 2px solid #fff;
+  padding: 20px;
+  border-radius: 20px;
+  height: 66px;
+  font-size: 16px;
+  color: #000;
+  background-color: #fff;
+  resize: none;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  &::placeholder {
+    font-size: 16px;
+  }
+  &:focus {
+    outline: none;
+    border-color: #1d9bf0;
+  }
+`;
+const EditNameButton = styled.button`
+  display: block;
+  background-color: tomato;
+  color: #fff;
+  border: 0;
+  font-weight: 600;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+export interface ITweet {
+  // 인테페이스 정의
+  id: string;
+  photo?: string; // 해당 타입이 문자열이거나 없을 수 있다
+  tweet: string;
+  userId: string;
+  username: string;
+  createdAt: number;
+  updateAt?: number;
+}
 
 export default function Profile() {
   const user = auth.currentUser;
   const [avartar, setAvatar] = useState(user?.photoURL);
   const [tweets, setTweets] = useState<ITweet[]>([]);
+  const [newName, setNewName] = useState("");
+  const [isEdit, setEditing] = useState(false);
+  const [errorEditName, setErrorEditName] = useState("");
 
   const onAvatarChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const { files } = e.target;
@@ -95,8 +140,34 @@ export default function Profile() {
     });
     setTweets(tweets);
   };
+  const onClickEditName = () => {
+    setEditing(true);
+  };
+  const onClickApplyName = async () => {
+    setErrorEditName("");
+    if (!user) {
+      return;
+    }
+    if (newName === "") {
+      setErrorEditName("Can't change to empty name.");
+      return;
+    }
+    await updateProfile(user, {
+      displayName: newName,
+    });
+    setEditing(false);
+  };
+  const onChangeEditNameField = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const {
+      target: { value },
+    } = e;
+    setNewName(value);
+  };
   useEffect(() => {
     fetchTweets();
+    if (user && user.displayName) {
+      setNewName(user.displayName);
+    }
   }, []);
 
   return (
@@ -126,7 +197,22 @@ export default function Profile() {
         accept="image/*"
       />
       {/* <Name>{user?.displayName ? user.displayName : "Anonymous"}</Name> */}
-      <Name>{user?.displayName ?? "Anonymous"}</Name>
+      {/* <Name>{user?.displayName ?? "Anonymous"}</Name> */}
+      {isEdit ? (
+        <>
+          <EditNameTextEField
+            onChange={onChangeEditNameField}
+            value={newName}
+          />
+          <EditNameButton onClick={onClickApplyName}>Apply</EditNameButton>
+          {errorEditName !== "" ? <Error>{errorEditName}</Error> : null}
+        </>
+      ) : (
+        <>
+          <Name>{user?.displayName ?? "Anonymous"}</Name>
+          <EditNameButton onClick={onClickEditName}>Edit</EditNameButton>
+        </>
+      )}
       <Tweets>
         {tweets.map((tweet) => (
           <Tweet key={tweet.id} {...tweet} />
